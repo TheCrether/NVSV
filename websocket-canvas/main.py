0fe48369d91294e6a5b1dc8f4dae1ecc03cc48ff
TheCrether <denisimeri6@gmail.com>
2021-11-11 15:21:01 +0100

    @0Adiber made HTTP and websockets run at the same time

--- websocket-canvas/main.py
+++ websocket-canvas/main.py
@@ -5,6 +5,8 @@ import websockets
 import http.server
 import socketserver
 
+from threading import Thread
+
 ###########################################
 # STATE: dict = {"canvas": [["#ffffff"]*800]*600} # this is wrong because it messes up references
 STATE: dict = {"canvas": [["#fff" for j in range(800)] for i in range(600)]}
@@ -15,11 +17,9 @@ PORT: int = 8080
 ###########################################
 
 
-class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.path = 'public/index.html'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory='public', **kwargs)
 
 
 def state_event():
@@ -34,10 +34,15 @@ def user_event():
     return json.dumps({"type": "user", "count": len(USERS)})
 
 
+async def broadcast(socks, data):
+    for ws in socks:
+        await ws.send(data)
+
+
 async def draw(websocket, path):
     try:
         USERS.add(websocket)
-        websockets.broadcast(USERS, user_event())
+        await broadcast(USERS, user_event())
         await websocket.send(state_event())
 
         async for message in websocket:
@@ -45,25 +50,33 @@ async def draw(websocket, path):
             if data['action'] == 'draw':
                 x, y = data['pos']
                 STATE["canvas"][y][x] = data['color']
-                websockets.broadcast(
-                    USERS, draw_event({key: data[key] for key in ['pos', 'color', 'width']}))
+                await broadcast(USERS, draw_event({key: data[key] for key in ['pos', 'color', 'width']}))
 
     finally:
         USERS.remove(websocket)
-        websockets.broadcast(USERS, user_event())
+        await broadcast(USERS, user_event())
 
 
-async def main():
-    # Create an object of the above class
-    handler = HttpRequestHandler
+def webserver():
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print('serving on port: ', PORT)
+        httpd.serve_forever()
 
-    # Start the server
-    # my_server = socketserver.TCPServer(("", PORT), handler)
-    # my_server.serve_forever()
 
+async def websocket():
     async with websockets.serve(draw, "localhost", 1337):
+        print('websockets listening on port: 1337')
         await asyncio.Future()
 
 
+def main():
+
+    # Start the server
+    Thread(target=webserver, args=[]).start()
+
+    # Start websocket
+    Thread(target=asyncio.run, args=[websocket()]).start()
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
